711e55b8e1fd772bb1c88dd826a9454f4b0e564a
tegarsweefer <tegar.s@weefer.co.id>
2020-04-09 14:19:53 +0700

    basic

---
+++ LearnNLog/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace LearnNLog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly Logger _logger;
+        public HomeController()
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.Info("HomeController.Get");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
